264b2bff1abe3a8fe52641a30fdbe98be54110ea
Nuno Pacheco <nunorpacheco@gmail.com>
2020-12-04 23:10:13 +0000

    Developing admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { signout } from './actions/userActions';
+import AdminRoute from './components/AdminRoute';
+import PrivateRoute from './components/PrivateRoute';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import ProductListScreen from './screens/ProductListScreen';
 import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -27,12 +33,12 @@ function App() {
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              amazona
+              Maria's Boutique
             </Link>
           </div>
           <div>
             <Link to="/cart">
-            Cart
+            Cesto
               {cartItems.length > 0 && (
                 <span className="badge">{cartItems.length}</span>
               )}
@@ -44,19 +50,52 @@ function App() {
                 </Link>
                 <ul className="dropdown-content">
                   <li>
+                      <Link to="/profile">User Profile</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderhistory">Histórico</Link>
+                  </li>
+                  <li>
                     <Link to="#signout" onClick={signoutHandler}>
-                      Sign Out
+                      Sair
                     </Link>
                   </li>
                 </ul>
               </div>
             ) : (
-              <Link to="/signin">Sign In</Link>
+              <Link to="/signin">Autenticar</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to="#admin">
+                  Admin <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to="/productlist">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/orderlist">Orders</Link>
+                  </li>
+                  <li>
+                    <Link to="/userlist">Users</Link>
+                  </li>
+                </ul>
+              </div>
             )}
           </div>
         </header>
         <main>
         <Route path="/cart/:id?" component={CartScreen}></Route>
+        <Route path="/product/:id" component={ProductScreen} exact></Route>
+        <Route
+            path="/product/:id/edit"
+            component={ProductEditScreen}
+            exact
+        ></Route>
         <Route path="/product/:id" component={ProductScreen}></Route>
         <Route path="/signin" component={SigninScreen}></Route>
         <Route path="/register" component={RegisterScreen}></Route>
@@ -64,6 +103,15 @@ function App() {
         <Route path="/payment" component={PaymentMethodScreen}></Route>
         <Route path="/placeorder" component={PlaceOrderScreen}></Route>
         <Route path="/order/:id" component={OrderScreen}></Route>
+        <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+        <PrivateRoute
+            path="/profile"
+            component={ProfileScreen}
+        ></PrivateRoute>
+        <AdminRoute
+            path="/productlist"
+            component={ProductListScreen}
+          ></AdminRoute>
         <Route path="/" component={HomeScreen} exact></Route>    
         </main>	      
         <footer className="row center">All right reserved</footer>	    	    
